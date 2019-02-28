ec5028e6fb1c340498b6a0394b0ed9a228835c38
rramo004 <rramos@Richards-MacBook-Air.local>
2019-02-27 22:24:38 -0800

    Add navbar, routing, and tabview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NoinputComponent } from './components/noinput/noinput.component';
-import { SecuritybarComponent } from './components/securitybar/securitybar.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,47 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoinputComponent } from './components/noinput/noinput.component';
-import { MatTableModule } from '@angular/material';
+import { MatTableModule, MatToolbarModule, MatTabsModule } from '@angular/material';
 import { SecuritybarComponent } from './components/securitybar/securitybar.component';
 import { TvsmComponent } from './components/tvsm/tvsm.component';
 
 import { JsonService } from './services/json.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Route1Component } from './components/navbar/routes/route1/route1.component';
+import { Route2Component } from './components/navbar/routes/route2/route2.component';
+import { TabviewComponent } from './components/tabview/tabview.component';
 
+const appRoutes: Routes = [
+  { path: 'route1', component: Route1Component },
+  { path: 'route2', component: Route2Component }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NoinputComponent,
     SecuritybarComponent,
-    TvsmComponent
+    TvsmComponent,
+    NavbarComponent,
+    Route1Component,
+    Route2Component,
+    TabviewComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
+    MatToolbarModule,
+    MatTabsModule,
     HttpClientModule
   ],
   providers: [
